81f80a9ecccd5884eb3225734ce242d3af2d0487
Leong Zhin Sheng <63205495+kelvinleong0529@users.noreply.github.com>
2021-03-16 18:39:55 +0800

    Time: 48 ms (58.20%), Space: 15.1 MB (40.33%) - LeetHub

---
+++ distance-between-bus-stops/distance-between-bus-stops.py
@@ -0,0 +1,5 @@
+class Solution:
+    def distanceBetweenBusStops(self, distance: List[int], start: int, destination: int) -> int:
+        if start > destination:
+            start,destination = destination,start
+        return min(sum(distance[start:destination]),sum(distance)-sum(distance[start:destination]))
