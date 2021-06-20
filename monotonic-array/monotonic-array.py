76361caf5ff913fce57f66147525127b3e032f98
Leong Zhin Sheng <63205495+kelvinleong0529@users.noreply.github.com>
2021-06-20 22:50:06 +0800

    Time: 464 ms (73.09%), Space: 20.9 MB (14.92%) - LeetHub

--- monotonic-array/monotonic-array.py
+++ monotonic-array/monotonic-array.py
@@ -1,13 +1,6 @@
 class Solution:
     def isMonotonic(self, nums: List[int]) -> bool:
-        if len(nums)==1:
-            return True
-        temp1 = nums[1:]
-        temp2 = nums[:-1]
-        ans = []
-        for i in range (len(temp1)):
-            ans.append(temp1[i]-temp2[i])
-        if max(ans)>0 and min(ans)<0:
-            return False
-        else:
-            return True
+        copy = nums.copy()
+        copy.sort()
+        return (nums == copy or nums == copy[::-1])
+            
