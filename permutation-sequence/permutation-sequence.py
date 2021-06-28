50d669323053e9b5870aa69716bc6853ad9097f8
Leong Zhin Sheng <63205495+kelvinleong0529@users.noreply.github.com>
2021-06-29 00:05:20 +0800

    Time: 32 ms (71.09%), Space: 14.3 MB (38.14%) - LeetHub

--- permutation-sequence/permutation-sequence.py
+++ permutation-sequence/permutation-sequence.py
@@ -1,6 +1,6 @@
 class Solution:
     def getPermutation(self, n: int, k: int) -> str:
-        def fac(n,table):
+        def fac(n,table): # define factorial function
             if n == 0:
                 return 1
             if n in table:
@@ -13,14 +13,12 @@ class Solution:
                 
         k -= 1
         remaining = [i+1 for i in range(n)]
-        size = n-1
         ans = []
         table = {1:1}
         for i in range(n):
-            fac_rslt = fac(size,table)
+            fac_rslt = fac(len(remaining)-1,table)
             index = k // fac_rslt
             ans.append(str(remaining[index]))
             remaining.pop(index)
             k %= fac_rslt
-            size -= 1
         return ''.join(ans)
