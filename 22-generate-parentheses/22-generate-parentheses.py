e34ae0cc3a88267a1fde1d55a71afcc28673db3d
Leong Zhin Sheng <zhinsheng97@hotmail.com>
2022-09-08 10:07:41 +0800

    Time: 70 ms (21.12%), Space: 14.3 MB (39.45%) - LeetHub

---
+++ 22-generate-parentheses/22-generate-parentheses.py
@@ -0,0 +1,22 @@
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
+        ans = []
+        stack = []
+        
+        def backtrack(openN,closeN):
+            if openN == closeN == n:
+                ans.append("".join(stack))
+                return
+            
+            if openN < n:
+                stack.append("(")
+                backtrack(openN+1,closeN)
+                stack.pop()
+            
+            if closeN < openN:
+                stack.append(")")
+                backtrack(openN,closeN+1)
+                stack.pop()
+            
+        backtrack(0,0)
+        return ans
