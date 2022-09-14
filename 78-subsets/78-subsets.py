90e74d9c675c5794f9034c784d47231d7943f82e
Leong Zhin Sheng <zhinsheng97@hotmail.com>
2022-09-14 23:53:40 +0800

    Time: 65 ms (24.71%), Space: 14 MB (82.42%) - LeetHub

--- 78-subsets/78-subsets.py
+++ 78-subsets/78-subsets.py
@@ -1,20 +1,22 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
+
         ans = []
         stack = []
         
-        def backtrack(i):
-            if i >= len(nums):
+        def backtracking(i):
+            if i == len(nums):
                 ans.append(stack.copy())
                 return
             
+            # NOT to include
+            backtracking(i+1)
+            
             # to include
             stack.append(nums[i])
-            backtrack(i+1)
+            backtracking(i+1)
             stack.pop()
             
-            # to NOT include
-            backtrack(i+1)
-            
-        backtrack(0)
+        backtracking(0)
+        
         return ans
