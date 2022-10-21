9a2e297f29d6435a9633cd6b5d6f669e180449bd
Leong Zhin Sheng <zhinsheng97@hotmail.com>
2022-10-21 16:59:07 +0800

    Time: 1360 ms (5.07%), Space: 39.4 MB (6.39%) - LeetHub

--- 1331-rank-transform-of-an-array/1331-rank-transform-of-an-array.py
+++ 1331-rank-transform-of-an-array/1331-rank-transform-of-an-array.py
@@ -2,16 +2,17 @@ class Solution:
     def arrayRankTransform(self, arr: List[int]) -> List[int]:
         hashMap = {}
         ans = [1]*len(arr)
+        
         for i,v in enumerate(arr):
             if v in hashMap:
                 hashMap[v].append(i)
             else:
                 hashMap[v] = [i]
-                
-
+        
         heapq.heapify(arr)
         count = 1
         previous = None
+        
         while arr:
             value = heapq.heappop(arr)
             if value == previous:
@@ -20,4 +21,6 @@ class Solution:
             for a in hashMap[value]:
                 ans[a] = count
             count += 1
+            
         return ans
+    
