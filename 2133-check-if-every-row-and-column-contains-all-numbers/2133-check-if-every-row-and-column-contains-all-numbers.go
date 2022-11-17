54f0f129f7df066a510ef78c883ea7cdfd842d34
Leong Zhin Sheng <zhinsheng97@hotmail.com>
2022-11-17 18:04:53 +0800

    Time: 167 ms (81.54%), Space: 7.3 MB (87.69%) - LeetHub

---
+++ 2133-check-if-every-row-and-column-contains-all-numbers/2133-check-if-every-row-and-column-contains-all-numbers.go
@@ -0,0 +1,39 @@
+package main
+
+func transpose(arr [][]int) [][]int {
+	var res = make([][]int, len(arr))
+	for i := 0; i < len(arr); i++ {
+		res[i] = make([]int, len(arr))
+		for j := 0; j < len(arr); j++ {
+			res[i][j] = arr[j][i]
+		}
+	}
+	return res
+}
+
+func check(row []int) bool {
+	hashMap := make(map[int]bool)
+	for i := range row {
+		_, ok := hashMap[row[i]]
+		if ok {
+			return false
+		}
+		hashMap[row[i]] = true
+	}
+	return true
+}
+
+func checkValid(matrix [][]int) bool {
+	for i := range matrix {
+		if !check(matrix[i]) {
+			return false
+		}
+	}
+	matrix = transpose(matrix)
+	for i := range matrix {
+		if !check(matrix[i]) {
+			return false
+		}
+	}
+	return true
+}
