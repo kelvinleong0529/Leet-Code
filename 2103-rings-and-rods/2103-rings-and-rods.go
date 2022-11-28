dca6f33091109c45c3f797b72ce5fed86eff8d3c
Leong Zhin Sheng <zhinsheng97@hotmail.com>
2022-11-29 01:37:30 +0800

    Time: 0 ms (100.00%), Space: 2 MB (60.78%) - LeetHub

--- 2103-rings-and-rods/2103-rings-and-rods.go
+++ 2103-rings-and-rods/2103-rings-and-rods.go
@@ -1,23 +1,40 @@
 func countPoints(rings string) int {
-    hashMap := make(map[byte][]bool)
     
-    colorMap := map[byte]int {'B':0,'G':1,'R':2}
+    type Color struct {
+        blue bool
+        green bool
+        red bool
+    }
+    hashMap := make(map[byte]Color)
+    // colorMap := map[byte]int {'B':0,'G':1,'R':2}
     
     for i:= 0; i < len(rings); i += 2 {
         color := rings[i]
         num := rings[i+1]
-        index := colorMap[color]
+        // index := colorMap[color]
         _, ok := hashMap[num]
         if !ok {
-            hashMap[num] = []bool {false, false, false}
+            hashMap[num] = Color{}
         }
-        hashMap[num][index] = true
+        if value, ok := hashMap[num]; ok {
+            switch color {
+            case 'B':
+                value.blue = true
+            case 'R':
+                value.red = true
+            case 'G':
+                value.green = true
+            }
+            hashMap[num] = value
+        }
+        // hashMap[num][index] = true
     }
     
     answer := 0
     
     for _, v := range hashMap {
-        if v[0] && v[1] && v[2] {
+        // if v[0] && v[1] && v[2] {
+        if v.blue && v.green && v.red {
             answer++
         }
     }
