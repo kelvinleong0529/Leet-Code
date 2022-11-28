c0b6cc98c03a1f4c60af35decf6080c9a74fd487
Leong Zhin Sheng <zhinsheng97@hotmail.com>
2022-11-29 01:37:49 +0800

    Time: 0 ms (100.00%), Space: 2 MB (60.78%) - LeetHub

--- 2103-rings-and-rods/2103-rings-and-rods.go
+++ 2103-rings-and-rods/2103-rings-and-rods.go
@@ -6,12 +6,10 @@ func countPoints(rings string) int {
         red bool
     }
     hashMap := make(map[byte]Color)
-    // colorMap := map[byte]int {'B':0,'G':1,'R':2}
     
     for i:= 0; i < len(rings); i += 2 {
         color := rings[i]
         num := rings[i+1]
-        // index := colorMap[color]
         _, ok := hashMap[num]
         if !ok {
             hashMap[num] = Color{}
@@ -27,13 +25,11 @@ func countPoints(rings string) int {
             }
             hashMap[num] = value
         }
-        // hashMap[num][index] = true
     }
     
     answer := 0
     
     for _, v := range hashMap {
-        // if v[0] && v[1] && v[2] {
         if v.blue && v.green && v.red {
             answer++
         }
