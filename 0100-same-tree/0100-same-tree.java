e71274062802c31a3c069d058988e8f4d3442d26
Leong Zhin Sheng <zhinsheng97@hotmail.com>
2024-04-20 19:39:40 +0800

    Time: 0 ms (100.00%), Space: 40.4 MB (95.42%) - LeetHub

---
+++ 0100-same-tree/0100-same-tree.java
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        return recursive(p, q);
+    }
+
+    private boolean recursive(TreeNode p, TreeNode q) {
+        if (p == null && q == null) {
+            return true;
+        }
+        if (p == null || q == null) {
+            return false;
+        }
+        return p.val == q.val && recursive(p.left, q.left) && recursive(p.right, q.right);
+    }
+}
