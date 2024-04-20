59a9a6b0e00c869556b2bb8d63d23ca2e51df06c
Leong Zhin Sheng <zhinsheng97@hotmail.com>
2024-04-20 18:50:17 +0800

    Time: 1 ms (76.91%), Space: 2.2 MB (57.41%) - LeetHub

---
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.go
@@ -0,0 +1,8 @@
+func strStr(haystack string, needle string) int {
+	for i := 0; i <= len(haystack)-len(needle); i++ {
+		if haystack[i:len(needle)+i] == needle {
+			return i
+		}
+	}
+	return -1
+}
