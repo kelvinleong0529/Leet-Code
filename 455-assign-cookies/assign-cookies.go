17fb163161f78df15c1f77b0d9f8b1ed7e065c96
Leong Zhin Sheng <zhinsheng97@hotmail.com>
2025-06-15 16:06:33 +0800

    Time: 6 ms (44.57%) | Memory: 9 MB (47.46%) - LeetSync

--- 455-assign-cookies/assign-cookies.go
+++ 455-assign-cookies/assign-cookies.go
@@ -1,47 +1,21 @@
-import (
-	"container/heap"
-)
-
-// MinHeap is a type that implements heap.Interface
-type MinHeap []int
-
-func (h MinHeap) Len() int            { return len(h) }
-func (h MinHeap) Less(i, j int) bool  { return h[i] < h[j] } // min-heap
-func (h MinHeap) Swap(i, j int)       { h[i], h[j] = h[j], h[i] }
-func (h *MinHeap) Push(x interface{}) { *h = append(*h, x.(int)) }
-func (h *MinHeap) Pop() interface{} {
-	old := *h
-	n := len(old)
-	val := old[n-1]
-	*h = old[0 : n-1]
-	return val
-}
-
 func findContentChildren(g []int, s []int) int {
-	greedHeap := new(MinHeap)
-	heap.Init(greedHeap)
-	for i := range g {
-		heap.Push(greedHeap, g[i])
-	}
-
-	cookiesHeap := new(MinHeap)
-	heap.Init(cookiesHeap)
-	for i := range s {
-		heap.Push(cookiesHeap, s[i])
-	}
-
+	sort.Ints(g)
+	sort.Ints(s)
+	
 	result := 0
+	gIndex := 0
+	sIndex := 0
+	
 	for {
-		if cookiesHeap.Len() == 0 || greedHeap.Len() == 0 {
+		if sIndex >= len(s) || gIndex >= len(g) {
 			break
 		}
-		cookie := heap.Pop(cookiesHeap).(int)
-		greed := (*greedHeap)[0]
-		if cookie >= greed {
-			heap.Pop(greedHeap)
-            result++
+		if s[sIndex] >= g[gIndex] {
+			gIndex++
+			result++
 		}
+		sIndex++
 	}
-
+	
 	return result
 }
