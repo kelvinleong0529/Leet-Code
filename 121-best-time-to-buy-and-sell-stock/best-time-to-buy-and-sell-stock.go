792fe4944daac7d058d1f3eb1583a37de93ea242
Leong Zhin Sheng <zhinsheng97@hotmail.com>
2025-08-18 00:40:00 +0800

    Time: 0 ms (100.00%) | Memory: 9.6 MB (80.36%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.go
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.go
@@ -1,19 +1,14 @@
 func maxProfit(prices []int) int {
 	lowest := 10_001
-	highest := -1
 	ans := 0
 	for _, price := range prices {
 		if price < lowest {
 			lowest = price
-			highest = -1
 			continue
 		}
-		if price > highest {
-			highest = price
-			profit := highest - lowest
-			if profit > ans {
-				ans = profit
-			}
+		profit := price - lowest
+		if profit > ans {
+			ans = profit
 		}
 	}
 
