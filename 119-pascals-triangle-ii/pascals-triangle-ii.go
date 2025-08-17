751af31b5132646495de5ddc47f82462b5077255
Leong Zhin Sheng <zhinsheng97@hotmail.com>
2025-08-17 17:55:28 +0800

    Time: 0 ms (100.00%) | Memory: 4.2 MB (22.32%) - LeetSync

---
+++ 119-pascals-triangle-ii/pascals-triangle-ii.go
@@ -0,0 +1,15 @@
+func getRow(rowIndex int) []int {
+	dp := make([][]int, 0)
+
+	for i := 0; i <= rowIndex; i++ {
+		row := make([]int, i+1)
+		for col := 1; col < len(row)-1; col++ {
+			row[col] = dp[i-1][col-1] + dp[i-1][col]
+		}
+		row[0] = 1
+		row[len(row)-1] = 1
+		dp = append(dp, row)
+	}
+
+	return dp[rowIndex]
+}
