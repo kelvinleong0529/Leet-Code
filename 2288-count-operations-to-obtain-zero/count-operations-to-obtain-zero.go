80816f19ac8e720881130e93f5f1bb93c245dc78
Leong Zhin Sheng <zhinsheng97@hotmail.com>
2025-08-20 20:50:12 +0800

    Time: 0 ms (100.00%) | Memory: 3.9 MB (38.10%) - LeetSync

--- 2288-count-operations-to-obtain-zero/count-operations-to-obtain-zero.go
+++ 2288-count-operations-to-obtain-zero/count-operations-to-obtain-zero.go
@@ -1,21 +1,13 @@
 func countOperations(num1 int, num2 int) int {
 	count := 0
-	for {
-        if num1 == 0 || num2 == 0 {
-            return count
-        }
+	for num1 > 0 && num2 > 0 {
 		if num1 >= num2 {
-			if num1%num2 == 0 {
-				return count + num1/num2
-			}
-			num1 -= num2
-			count++
+			count += num1 / num2
+			num1 %= num2
 		} else {
-			if num2%num1 == 0 {
-				return count + num2/num1
-			}
-			num2 -= num1
-			count++
+			count += num2 / num1
+			num2 %= num1
 		}
 	}
+	return count
 }
