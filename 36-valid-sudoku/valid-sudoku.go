09fc43a8c0b0a58388da895b53a973b16c531c12
Leong Zhin Sheng <zhinsheng97@hotmail.com>
2026-04-22 23:37:49 +0800

    Time: 0 ms (100.00%) | Memory: 4.8 MB (47.15%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.go
+++ 36-valid-sudoku/valid-sudoku.go
@@ -1,38 +1,28 @@
 func isValidSudoku(board [][]byte) bool {
-	
-	row := make([][]int, 9)
-	col := make([][]int, 9)
-	square := make([][][]int, 3)
+	row := make([]int, 9)
+	col := make([]int, 9)
+	square := make([][]int, 3)
 
-	for r := range len(board) {
-		row[r] = make([]int, 9)
-		col[r] = make([]int, 9)
-		for c := range len(board[0]) {
-			squareRow := r / 3
-			squareCol := c / 3
-            if square[squareRow] == nil {
-                square[squareRow] = make([][]int, 3)
-            }
-			if square[squareRow][squareCol] == nil {
-				square[squareRow][squareCol] = make([]int, 9)
-			}
-		}
+	for i := range len(square) {
+		square[i] = make([]int, 3)
 	}
 
 	for r := range len(board) {
-		for c := range len(board[0]) {
+		for c := range len(board[r]) {
 			if board[r][c] == '.' {
 				continue
 			}
-            num := board[r][c] - '0' - 1
+			num := board[r][c] - '0' - 1
 			squareRow := r / 3
 			squareCol := c / 3
-			if row[r][num] != 0 || col[c][num] != 0 || square[squareRow][squareCol][num] != 0 {
+			if row[r]&(1<<num) != 0 ||
+				col[c]&(1<<num) != 0 ||
+				square[squareRow][squareCol]&(1<<num) != 0 {
 				return false
 			}
-			row[r][num] = 1
-			col[c][num] = 1
-			square[squareRow][squareCol][num] = 1
+			row[r] |= (1 << num)
+			col[c] |= (1 << num)
+			square[squareRow][squareCol] |= (1 << num)
 		}
 	}
 
