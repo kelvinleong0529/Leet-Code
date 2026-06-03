77fa25b5f0641291bdef6cf24a76d0932d2ac652
Leong Zhin Sheng <zhinsheng97@hotmail.com>
2026-06-03 22:18:42 +0800

    Time: 5 ms (39.23%) | Memory: 4.7 MB (58.37%) - LeetSync

--- 242-valid-anagram/valid-anagram.go
+++ 242-valid-anagram/valid-anagram.go
@@ -1,22 +1,20 @@
 func isAnagram(s string, t string) bool {
-	hashMap := getHashMap(s)
-	for _, ch := range t {
-		count, ok := hashMap[ch]
-		if !ok {
-			return false
-		}
-		hashMap[ch] = count - 1
-		if hashMap[ch] == 0 {
-			delete(hashMap, ch)
-		}
-	}
-	return len(hashMap) == 0
-}
-
-func getHashMap(s string) map[rune]int {
 	hashMap := make(map[rune]int)
-	for _, ch := range s {
-		hashMap[ch]++
+
+	for _, char := range s {
+		hashMap[char]++
 	}
-	return hashMap
+
+	for _, char := range t {
+		if _, ok := hashMap[char]; !ok {
+			return false
+		} else {
+			hashMap[char]--
+			if hashMap[char] == 0 {
+				delete(hashMap, char)
+			}
+		}
+	}
+
+	return len(hashMap) == 0
 }
