545700a7abac010be95d83339ee1c454d09ad6e3
Leong Zhin Sheng <zhinsheng97@hotmail.com>
2026-06-09 22:25:53 +0800

    Time: 1 ms (5.41%) | Memory: 4.9 MB (13.54%) - LeetSync

--- 46-permutations/permutations.go
+++ 46-permutations/permutations.go
@@ -1,18 +1,19 @@
 func permute(nums []int) [][]int {
-    if len(nums) == 0 {
-        return [][]int {{}}
-    }
+	if len(nums) == 0 {
+		return [][]int{{}}
+	}
 
-    perms := permute(nums[1:])
-    ans := make([][]int, 0)
-    for _, v := range perms {
-        for i := range len(v) + 1 {
-            deepCopy := make([]int, len(v))
-            copy(deepCopy, v)
-            permutation := slices.Insert(deepCopy, i, nums[0])
-            ans = append(ans, permutation)
-        }
-    }
+	ans := make([][]int, 0)
+	perms := permute(nums[1:])
 
-    return ans
+	for _, v := range perms {
+		for i := 0; i <= len(v); i++ {
+			temp := make([]int, len(v))
+			copy(temp, v)
+			temp = slices.Insert(temp, i, nums[0])
+			ans = append(ans, temp)
+		}
+	}
+
+	return ans
 }
